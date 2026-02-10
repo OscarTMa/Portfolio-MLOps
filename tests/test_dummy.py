2c9e09e5d86b46f505bcd23115190a59ec649300
OscarTMmax <oscartibaduizam@gmail.com>
2026-02-10 18:22:51 +0100

    Agregando test dummy para arreglar exit code 5

---
+++ tests/test_dummy.py
@@ -0,0 +1,8 @@
+# tests/test_dummy.py
+
+def test_siempre_pasa():
+    """
+    Este test no hace nada útil, solo sirve para verificar 
+    que el pipeline de CI/CD está funcionando.
+    """
+    assert True
